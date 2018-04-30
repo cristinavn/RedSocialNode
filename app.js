22358fe866cd054e1e2f942a043d35e1681be8de
cristinavn <venanar@hotmail.com>
2018-04-30 10:18:05 +0200

    Login con token
    Login con token, con uso de cookies.

--- app.js
+++ app.js
@@ -2,6 +2,18 @@
 var express = require('express');	//función* u objeto
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    // Debemos especificar todas las headers que se aceptan. Content-Type , token
+    next();
+});
+
+var jwt = require('jsonwebtoken');
+app.set('jwt',jwt);
+
 var expressSession = require('express-session');
 app.use(expressSession({
     secret: 'abcdefg',
@@ -19,6 +31,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var gestorBD = require("./modules/gestorBD.js");
 gestorBD.init(app,mongo);
+
+// routerUsuarioToken
+var routerUsuarioToken = express.Router();
+routerUsuarioToken.use(function(req, res, next) {
+    // obtener el token, puede ser un parámetro GET , POST o HEADER
+    var token = req.body.token || req.query.token || req.headers['token'];
+    if (token != null) {
+        // verificar el token
+        jwt.verify(token, 'secreto', function(err, infoToken) {
+            if (err || (Date.now()/1000 - infoToken.tiempo) > 240 ){
+                res.status(403); // Forbidden
+                res.json({
+                    acceso : false,
+                    error: 'Token invalido o caducado'
+                });
+                // También podríamos comprobar que intoToken.usuario existe
+                return;
+
+            } else {
+                // dejamos correr la petición
+                res.usuario = infoToken.usuario;
+                next();
+            }
+        });
+
+    } else {
+        res.status(403); // Forbidden
+        res.json({
+            acceso : false,
+            mensaje: 'No hay Token'
+        });
+    }
+});
+// Aplicar routerUsuarioToken
+app.use('/api/cancion', routerUsuarioToken);
 
 // routerUsuarioSession
 var routerUsuarioSession = express.Router();
@@ -100,6 +147,7 @@ app.set('crypto',crypto);
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app, swig, gestorBD);
 require("./routes/rcanciones.js")(app, swig, gestorBD);
+require("./routes/rapiusuarios.js")(app, gestorBD);
 
 app.get('/', function (req, res) {
     res.redirect('/tienda');
---
+++ routes/rapiusuarios.js
@@ -0,0 +1,130 @@
+module.exports = function(app, gestorBD){
+
+    app.get("/api/cancion", function(req,res){
+        gestorBD.obtenerCanciones( {}, function(canciones) {
+            if (canciones == null) {
+                res.status(500);
+                res.json( {
+                    error : "Se ha producido un error"
+                })
+            } else {
+                res.status(200);
+                res.send(JSON.stringify(canciones));
+            }
+        })
+    })
+
+    app.put("/api/cancion/:id", function(req, res) {
+
+        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
+
+        var cancion = {}; // Solo los atributos a modificar
+        if ( req.body.nombre != null)
+            cancion.nombre = req.body.nombre;
+        if ( req.body.genero != null)
+            cancion.genero = req.body.genero;
+        if ( req.body.precio != null)
+            cancion.precio = req.body.precio;
+        gestorBD.modificarCancion(criterio, cancion, function(result) {
+            if (result == null) {
+                res.status(500);
+                res.json({
+                    error : "se ha producido un error"
+                })
+            } else {
+                res.status(200);
+                res.json({
+                    mensaje : "canción modificada",
+                    _id : req.params.id
+                })
+            }
+        });
+    });
+
+
+    app.get("/api/cancion/:id", function(req, res) {
+        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
+
+        gestorBD.obtenerCanciones(criterio,function(canciones){
+            if ( canciones == null ){
+                res.status(500);
+                res.json({
+                    error : "se ha producido un error"
+                })
+            } else {
+                res.status(200);
+                res.send( JSON.stringify(canciones[0]) );
+            }
+        });
+    });
+
+
+    app.delete("/api/cancion/:id", function(req, res) {
+        var criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id)}
+
+        gestorBD.eliminarCancion(criterio,function(canciones){
+            if ( canciones == null ){
+                res.status(500);
+                res.json({
+                    error : "se ha producido un error"
+                })
+            } else {
+                res.status(200);
+                res.send( JSON.stringify(canciones) );
+            }
+        });
+    });
+
+    app.post("/api/cancion", function(req, res) {
+        var cancion = {
+            nombre : req.body.nombre,
+            genero : req.body.genero,
+            precio : req.body.precio,
+        }
+        // ¿Validar nombre, genero, precio?
+
+        gestorBD.insertarCancion(cancion, function(id){
+            if (id == null) {
+                res.status(500);
+                res.json({
+                    error : "se ha producido un error"
+                })
+            } else {
+                res.status(201);
+                res.json({
+                    mensaje : "canción insertarda",
+                    _id : id
+                })
+            }
+        });
+
+    });
+
+    app.post("/api/autenticar/", function(req,res) {
+        var seguro = app.get("crypto").createHmac('sha256', app.get('clave'))
+            .update(req.body.password).digest('hex');
+
+        var criterio = {
+            email : req.body.email,
+            password : seguro
+        }
+
+        gestorBD.obtenerUsuarios(criterio, function(usuarios) {
+            if (usuarios == null || usuarios.length == 0) {
+                res.status(401); //Unauthorized
+                res.json({
+                    autenticado : false
+                });
+            } else {
+                var token = app.get('jwt').sign(
+                    {usuario: criterio.email , tiempo: Date.now()/1000},
+                    "secreto");
+                res.status(200);
+                res.json({
+                    autenticado: true,
+                    token : token
+                });
+            }
+        });
+    });
+}
